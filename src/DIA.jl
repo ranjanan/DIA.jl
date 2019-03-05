156502d31f67ac902c82c50e0dd1ac69b09cd519
ranjanan <benditlikeranjan@gmail.com>
2019-03-05 16:02:47 -0500

    Make diags be dense by default

--- src/DIA.jl
+++ src/DIA.jl
@@ -105,18 +105,18 @@ Base.Matrix(s::SparseMatrixDIA) = diagm(s.diags...)
 # TODO: Speed this up
 function SparseMatrixDIA(S::SparseMatrixCSC{Tv,Ti}) where {Tv,Ti}
     m,n = size(S)
-    s = Vector{Pair{Ti,SparseVector{Tv,Ti}}}()
+    s = Vector{Pair{Ti,Vector{Tv}}}()
     # Add diagonals above
     for i = 1:n
         d = diag(S, i-1)
         if nnz(d) != 0
-            push!(s, i-1 => d)
+            push!(s, i-1 => Vector(d))
         end
     end
     for i = -1:-1:-n
         d = diag(S, i)
         if nnz(d) != 0
-            push!(s, i => d)
+            push!(s, i => Vector(d))
         end
     end
     SparseMatrixDIA(tuple(s...), m, n)
