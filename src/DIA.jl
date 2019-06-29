328607cbd98aa83f0b51566d3c891b6f21b677bb
Ranjan Anantharaman <ranjan.anantharaman@gmail.com>
2019-06-29 16:34:00 -0400

    Finalize printing

--- src/DIA.jl
+++ src/DIA.jl
@@ -42,17 +42,35 @@ function Base.getindex(a::SparseMatrixDIA{Tv,Ti,N}, i, j) where {Tv,Ti,N}
     return zero(Tv)
 end
 
-Base.show(io::IO, S::SparseMatrixDIA) = Base.show(convert(IOContext, io), S::SparseMatrixDIA)
-function Base.show(io::IOContext, S::SparseMatrixDIA)
+# Base.show(io::IO, S::SparseMatrixDIA) = Base.show(convert(IOContext, io), S::SparseMatrixDIA)
 
-    println(io, summary(S))
-    for x in S.diags
+function Base.show(io::IO, p::Pair{Ti, Vector{Tv}}) where {Ti,Tv}
+	print(IOContext(io, :limit=>true), "Diagonal $(p.first): ", p.second)
+end 
+Base.summary(io::IO, v::Vector{Pair{Ti,Vector{Tv}}}) where {Ti,Tv} = ""
+
+#=function Base.show(io::IO, diags::Vector{Pair{Ti,Vector{Tv}}}) where {Ti,Tv}
+#	for i = 1:length(diags)
+#		print(io, diags[i], '\n')
+#	end
+	print(io, summary(diags), '\n')
+	show(IOContext(io, :compact=>true, :limit=>true), "text/plain", diags)
+end=#
+function Base.show(io::IO, S::SparseMatrixDIA)
+	print(io, summary(S))
+	print(io, '\n')
+	println(IOContext(io, :limit=>true), S.diags)
+end
+
+    #=for x in S.diags
         print(io, "Diagonal $(x.first): ")
         print(IOContext(io, :limit => true, :compact => true), x.second)
         print(io, '\n')
-    end
+    end=#
 
-end
+
+	
+
 Base.display(S::SparseMatrixDIA) = Base.show(S)
 
 function Base.summary(S::SparseMatrixDIA{Tv,Ti,V}) where {Tv,Ti,V}
